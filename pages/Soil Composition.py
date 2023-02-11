a2d414717d8f0807a053d6bee3823594d574a310
Saalim Quadri <danascape@gmail.com>
2023-02-12 03:45:31 +0530

    app: Add basic soil info

--- pages/Soil Composition.py
+++ pages/Soil Composition.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title("Soil Composition")
+
+st.write("Alluvial soils: These are formed by the deposition of sediments by rivers and are found in the Indo-Gangetic plain. They are rich in organic matter and minerals such as calcium, magnesium, and potassium.")
+
+st.write("Black soils: Also known as regur or black cotton soils, these are found in the Deccan plateau region. They are rich in iron, magnesium, and aluminum but are deficient in nitrogen, phosphorus, and organic matter.")
+
+st.write("Red soils: These are mainly found in the Deccan plateau and are formed by the weathering of igneous rocks. They are deficient in nitrogen, phosphorus, and organic matter, but are rich in iron oxide.")
+
+st.write("Laterite soils: These are found in the coastal regions of India and are formed by the weathering of rocks in hot and humid conditions. They are rich in iron and aluminum but are deficient in nitrogen, phosphorus, and organic matter.")
+
+st.write("Forest and mountain soils: These are found in the hilly regions of India and are formed by the weathering of rocks. They are rich in organic matter and minerals such as potassium, magnesium, and calcium.")
