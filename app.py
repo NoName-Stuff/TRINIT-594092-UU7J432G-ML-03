63bd5e648b3444121e7e28df336ba78d673aafc9
Saalim Quadri <danascape@gmail.com>
2023-02-11 21:59:18 +0530

    app: Import required python modules
    Signed-off-by: danascape <danascape@gmail.com>

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Tri NIT Hackathon")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
