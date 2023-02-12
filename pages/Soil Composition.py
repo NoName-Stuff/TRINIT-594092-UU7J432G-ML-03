537f5e716865944f8dd4e3654ba410d53731b1a9
Saalim Quadri <danascape@gmail.com>
2023-02-12 05:46:49 +0530

    Update Soil Composition.py

--- pages/Soil Composition.py
+++ pages/Soil Composition.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("Soil Composition")
 
+st.write("Soil is a critical resource for agriculture, providing the necessary nutrients and support for plant growth. India has a diverse range of soils, with varying compositions and characteristics that affect crop productivity. This report provides an overview of the major soil types in India and their composition.")
+
 st.write("Alluvial soils: These are formed by the deposition of sediments by rivers and are found in the Indo-Gangetic plain. They are rich in organic matter and minerals such as calcium, magnesium, and potassium.")
 
 st.write("Black soils: Also known as regur or black cotton soils, these are found in the Deccan plateau region. They are rich in iron, magnesium, and aluminum but are deficient in nitrogen, phosphorus, and organic matter.")
